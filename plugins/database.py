21a0d852c3b22f2ead59eac16b619a33c42804a1
rocky2109 <rockys.2109rs@gmail.com>
2025-05-17 13:27:16 +0530

    Add files via upload

---
+++ plugins/database.py
@@ -0,0 +1,50 @@
+import motor.motor_asyncio
+from config import DB_NAME, DB_URI
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id, name):
+        return dict(
+            id=id,
+            name=name,
+            session=None,
+        )
+
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        return await self.col.count_documents({})
+
+    async def get_all_users(self):
+        return self.col.find({})  # This returns an AsyncIOMotorCursor
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_session(self, id, session):
+        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
+
+    async def get_session(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('session') if user else None
+  
+    # Example for MongoDB:
+  
+    async def get_required_keyword(chat_id):
+        data = await keywords_collection.find_one({"chat_id": chat_id})
+        return data.get("keyword") if data else None
+
+
+# Initialize the database instance
+db = Database(DB_URI, DB_NAME)
